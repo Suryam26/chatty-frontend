e86d1d35b674050fc972527b55037c6f222c872f
Suryam Jain <suryam@Suryams-MacBook-Pro.local>
2024-07-30 08:36:40 +0530

    fix: delete message

--- src/contexts/NotificationContext.tsx
+++ src/contexts/NotificationContext.tsx
@@ -1,104 +1,104 @@
-import React, { createContext, ReactNode, useContext, useState } from "react";
-import useWebSocket, { ReadyState } from "react-use-websocket";
+import React, {createContext, ReactNode, useContext, useState} from "react";
+import useWebSocket, {ReadyState} from "react-use-websocket";
 
-import { AuthContext } from "./AuthContext";
+import {AuthContext} from "./AuthContext";
 
 const DefaultProps = {
-  unreadMessageCount: 0,
-  unreadCountList: [],
-  typing: {},
-  connectionStatus: "Uninstantiated",
+    unreadMessageCount: 0,
+    unreadCountList: [],
+    typing: {},
+    connectionStatus: "Uninstantiated",
 };
 
 export interface UnreadCountListProps {
-  from_user__username: string;
-  count: number;
+    from_user__username: string;
+    count: number;
 }
 
 export interface NotificationProps {
-  unreadMessageCount: number;
-  unreadCountList: UnreadCountListProps[];
-  typing: any;
-  connectionStatus: string;
+    unreadMessageCount: number;
+    unreadCountList: UnreadCountListProps[];
+    typing: any;
+    connectionStatus: string;
 }
 
 export const NotificationContext =
-  createContext<NotificationProps>(DefaultProps);
+    createContext<NotificationProps>(DefaultProps);
 
 export const NotificationContextProvider: React.FC<{ children: ReactNode }> = ({
-  children,
-}) => {
-  const { user } = useContext(AuthContext);
-  const [typing, setTyping] = useState(false);
-  const [unreadMessageCount, setUnreadMessageCount] = useState(0);
-  const [unreadCountList, setUnreadCountList] = useState<
-    UnreadCountListProps[]
-  >([]);
+                                                                                   children,
+                                                                               }) => {
+    const {user} = useContext(AuthContext);
+    const [typing, setTyping] = useState(false);
+    const [unreadMessageCount, setUnreadMessageCount] = useState(0);
+    const [unreadCountList, setUnreadCountList] = useState<
+        UnreadCountListProps[]
+    >([]);
 
-  const { readyState } = useWebSocket(
-    user ? `ws://127.0.0.1:8000/notifications/` : null,
-    {
-      queryParams: {
-        token: user ? user.token : "",
-      },
-      onOpen: () => {
-        console.log("Connected to Notifications!");
-      },
-      onClose: () => {
-        console.log("Disconnected from Notifications!");
-      },
-      onMessage: (e) => {
-        const data = JSON.parse(e.data);
-        switch (data.type) {
-          case "unread_count":
-            setUnreadMessageCount(data.unread_count);
-            setUnreadCountList(data.unread_count_list);
-            break;
-          case "new_message_notification":
-            setUnreadMessageCount((count) => (count += 1));
-            updateCountList(data.name);
-            break;
-          case "typing_notification":
-            setTyping(data);
-            break;
-          case "delete_message":
-            setUnreadCountList([...unreadCountList]);
-            break;
-          default:
-            console.log("Unknown message type!");
-            break;
+    const {readyState} = useWebSocket(
+        user ? `ws://127.0.0.1:8000/notifications/` : null,
+        {
+            queryParams: {
+                token: user ? user.token : "",
+            },
+            onOpen: () => {
+                console.log("Connected to Notifications!");
+            },
+            onClose: () => {
+                console.log("Disconnected from Notifications!");
+            },
+            onMessage: (e) => {
+                const data = JSON.parse(e.data);
+                switch (data.type) {
+                    case "unread_count":
+                        setUnreadMessageCount(data.unread_count);
+                        setUnreadCountList(data.unread_count_list);
+                        break;
+                    case "new_message_notification":
+                        setUnreadMessageCount((count) => (count += 1));
+                        updateCountList(data.name);
+                        break;
+                    case "typing_notification":
+                        setTyping(data);
+                        break;
+                    case "delete_message":
+                        setUnreadCountList(unreadCountList ? [...unreadCountList] : []);
+                        break;
+                    default:
+                        console.log("Unknown message type!");
+                        break;
+                }
+            },
         }
-      },
-    }
-  );
-
-  function updateCountList(username: string) {
-    const unreadCountListCopy =
-      unreadCountList?.length > 0 ? [...unreadCountList] : [];
-    const userIndex = unreadCountListCopy.findIndex(
-      (e) => e.from_user__username === username
     );
-    if (userIndex === -1) {
-      unreadCountListCopy.push({ from_user__username: username, count: 1 });
-    } else {
-      unreadCountListCopy[userIndex].count++;
+
+    function updateCountList(username: string) {
+        const unreadCountListCopy =
+            unreadCountList?.length > 0 ? [...unreadCountList] : [];
+        const userIndex = unreadCountListCopy.findIndex(
+            (e) => e.from_user__username === username
+        );
+        if (userIndex === -1) {
+            unreadCountListCopy.push({from_user__username: username, count: 1});
+        } else {
+            unreadCountListCopy[userIndex].count++;
+        }
+        setUnreadCountList(unreadCountListCopy);
     }
-    setUnreadCountList(unreadCountListCopy);
-  }
 
-  const connectionStatus = {
-    [ReadyState.CONNECTING]: "Connecting",
-    [ReadyState.OPEN]: "Open",
-    [ReadyState.CLOSING]: "Closing",
-    [ReadyState.CLOSED]: "Closed",
-    [ReadyState.UNINSTANTIATED]: "Uninstantiated",
-  }[readyState];
+    const connectionStatus = {
+        [ReadyState.CONNECTING]: "Connecting",
+        [ReadyState.OPEN]: "Open",
+        [ReadyState.CLOSING]: "Closing",
+        [ReadyState.CLOSED]: "Closed",
+        [ReadyState.UNINSTANTIATED]: "Uninstantiated",
+    }[readyState];
 
-  return (
-    <NotificationContext.Provider
-      value={{ unreadMessageCount, unreadCountList, typing, connectionStatus }}
-    >
-      {children}
-    </NotificationContext.Provider>
-  );
+    return (
+        <NotificationContext.Provider
+            value={{unreadMessageCount, unreadCountList, typing, connectionStatus}}
+        >
+            {children}
+        </NotificationContext.Provider>
+    );
 };
